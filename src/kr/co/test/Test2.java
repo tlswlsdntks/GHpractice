589f5b1608a049e952981c1211f7c3b47647af48
tlswl <tlswl@121.164.230.67>
2021-07-04 15:40:37 +0900

    22

--- src/kr/co/test/Test2.java
+++ src/kr/co/test/Test2.java
@@ -4,6 +4,11 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Test 2!!");
+		System.out.println("Test 2!!");
+		System.out.println("Test 2!!");
+		System.out.println("Test 2!!");
+		System.out.println("Test 2!!");
+		System.out.println("Test 2!!");
 	}
 
 }
